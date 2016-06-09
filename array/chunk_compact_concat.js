0e7bf9502595bdac67a06393e2eac5ed50d482a7
Max Stern <stern.maxwell@gmail.com>
2016-06-09 16:15:07 -0500

    index

---
+++ array/chunk_compact_concat.js
@@ -0,0 +1,12 @@
+// Creates an array of elements split into groups the length of size. If array can’t be split evenly, the final chunk will be the remaining elements.
+
+var array = [1,2,3,4,5];
+_.chunk(array,3);
+
+
+
+_.chunk(['a', 'b', 'c', 'd'], 2);
+// → [['a', 'b'], ['c', 'd']]
+
+_.chunk(['a', 'b', 'c', 'd'], 3);
+// → [['a', 'b', 'c'], ['d']]
