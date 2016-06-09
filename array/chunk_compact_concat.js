7b3717c2d53697854e9ff3698d6ea22066233643
Max Stern <stern.maxwell@gmail.com>
2016-06-09 16:19:30 -0500

    _.compact

--- array/chunk_compact_concat.js
+++ array/chunk_compact_concat.js
@@ -1,8 +1,8 @@
-// Creates an array of elements split into groups the length of size. If array can’t be split evenly, the final chunk will be the remaining elements.
+// CHUNK Creates an array of elements split into groups the length of size. If array can’t be split evenly, the final chunk will be the remaining elements.
 
 var array = [1,2,3,4,5];
 _.chunk(array,3);
-
+// →
 
 
 _.chunk(['a', 'b', 'c', 'd'], 2);
@@ -10,3 +10,9 @@ _.chunk(['a', 'b', 'c', 'd'], 2);
 
 _.chunk(['a', 'b', 'c', 'd'], 3);
 // → [['a', 'b', 'c'], ['d']]
+
+
+// COMPACT Creates an array with all falsey values removed. The values false, null, 0, "", undefined, and NaN are falsey.
+
+_.compact([0, 1, false, 2, '', 3]);
+// → [1, 2, 3]
