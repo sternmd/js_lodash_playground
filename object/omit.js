e4bb225ec02aec02ad02b85b7606054863692c4a
Max Stern <stern.maxwell@gmail.com>
2016-06-10 14:23:17 -0500

    _.omit

--- object/omit.js
+++ object/omit.js
@@ -13,3 +13,12 @@ objA; // {"name": "colin"}
 
 // Lodash
 objA = _.omit(objA, ['car', 'age']); // {"name": "colin"}
+
+
+// Other instances
+
+var objA = {"name": "colin", "car": "suzuki", "age": 17};
+
+// Lodash
+objA = _.omit(objA, 'car'); // {"name": "colin", "age": 17};
+objA = _.omit(objA, _.isNumber); // {"name": "colin"};
