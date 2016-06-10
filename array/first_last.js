bdd9aab2cae8bd7524f39540a5ea65dcb452cfac
Max Stern <stern.maxwell@gmail.com>
2016-06-10 13:44:17 -0500

    _.first, _.last

---
+++ array/first_last.js
@@ -0,0 +1,19 @@
+// Get the first element of array.
+
+array = [1, 2, 3];
+
+_.first(array)
+// → 1
+
+_.head([1, 2, 3]);
+// → 1
+
+_.head([]);
+// → undefined
+
+// Get the last element of array.
+_.last([1, 2, 3]);
+// → 3
+
+_.last(array);
+// → 3
