d77e143fe51198f8a055cc6b952fc482f261d150
Max Stern <stern.maxwell@gmail.com>
2016-06-10 12:00:38 -0500

    while loops

--- array/for_loop.js
+++ array/for_loop.js
@@ -4,6 +4,22 @@
 var friends = ["Mando", "Tim", "Alex", "Aidan", "Anthony"];
 
 // Now, create a loop that will pop a friend out every time the loop runs.
-  for (var i = 0; i < friends.length) {
+  for (var i = 0; i < friends.length; i++) {
     return friends[i].pop();
   }
+
+
+  // 1. Basic for loop.
+  for(var i = 0; i < friends.length; i++) {
+      // ....
+  }
+
+  // 2. Using Array's join and split methods
+  Array.apply(null, Array(5)).forEach(function(){
+      // ...
+  });
+
+  // Lodash
+  _.times(5, function(){
+      // ...
+  });
---
+++ array/while_loop.js
@@ -0,0 +1,25 @@
+// Runs its code as long as its boolean expression is true
+
+// executes 5 times, prints numbers 1 to 5 in ascending order
+
+var number = 1;
+while(number <= 5) {
+  console.log(number);
+  number++;
+}
+
+// initialize first train, but only 8 trains running, until 9 -> false so loop stops
+var trainNumber = 1;
+while(trainNumber <=8{
+  console.log("Train #" + trainNumber + " is running");
+  trainNumber++;
+})
+
+// Using variables instead of values
+var trainsOperational = 8;
+var trainNumber = 1;
+
+while (trainNumber <= trainsOperational) {
+  console.log("Train #" + trainNumber + " is running");
+  trainNumber++;
+}
